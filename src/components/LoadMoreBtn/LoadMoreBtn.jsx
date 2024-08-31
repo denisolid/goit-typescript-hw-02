02c80ce3be04a15aa14ed34fad151180592a0a1b
denisolid <dkolisnichienko321@gmail.com>
2024-08-31 01:18:29 +0000

    preparing

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++
@@ -1,5 +0,0 @@
-const LoadMoreBtn = ({ onClick }) => {
-  return <button onClick={onClick}>Load more</button>;
-};
-
-export default LoadMoreBtn;
--- src/components/Loader/Loader.jsx
+++
@@ -1,7 +0,0 @@
-import { TailSpin } from "react-loader-spinner";
-
-const Loader = () => {
-  return <TailSpin color="#00BFFF" height={80} width={80} />;
-};
-
-export default Loader;
